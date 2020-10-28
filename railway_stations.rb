3f74c76a1ab80e64129c6e6df97e0ea44a6b7deb
banpresto <sexyteemo@gmail.com>
2020-10-28 09:40:53 +0300

    lesson 3

--- railway_stations.rb
+++ railway_stations.rb
@@ -1,25 +1,21 @@
 class Station
-  attr_reader :name, :train
+  attr_reader :name, :trains
 
   def initialize(name)
     @name = name
-    @train = []
+    @trains = []
   end
 
   def add_train(train)
-    @train << train
+    @trains << train
   end
 
-  def list_of_trains
-    @train.each { |train| puts train }
-  end
-
-  def list_of_trains_by_type
-    @train.each { |train| puts train.type }
+  def list_of_trains_by_type(type)
+    @trains.select { |train| train.type == type }
   end
 
   def delete_train(train)
-    @train.delete(train)
+    @trains.delete(train)
   end
 end
 
@@ -42,7 +38,7 @@ class Route
 end
 
 class Train
-  attr_accessor :speed, :train_in_station
+  attr_accessor :speed, :current_station
 
   attr_reader :number_of_cars, :type
 
@@ -51,7 +47,7 @@ class Train
     @type = type
     @number_of_cars = number_of_cars
     @speed = 0
-    @train_in_station  
+    @current_station  
   end
 
   def stop
@@ -68,25 +64,28 @@ class Train
 
   def add_train_to_route(way)
     @way = way
-    @train_in_station = way.starting_station
-    @train_in_station.add_train(self)
+    @current_station = way.starting_station
+    @current_station.add_train(self)
   end
 
   def move_forward
-    @train_in_station.delete_train(self)
-    @train_in_station = @way.route[@way.route.index(train_in_station) + 1] if @train_in_station != @way.terminal_station
-    @train_in_station.add_train(self)   
+    @current_station.delete_train(self)
+    @current_station = @way.route[@way.route.index(current_station) + 1] if @train_in_station != @way.terminal_station
+    @current_station.add_train(self)   
   end
 
   def move_back
-    @train_in_station.delete_train(self)
-    @train_in_station = @way.route[@way.route.index(train_in_station) - 1] if @train_in_station != @way.starting_station
-    @train_in_station.add_train(self)
+    @current_station.delete_train(self)
+    @current_station = @way.route[@way.route.index(current_station) - 1] if @train_in_station != @way.starting_station
+    @current_station.add_train(self)
   end
 
-  def current_station
-    next_station = @way.route[@way.route.index(train_in_station) + 1] 
-    last_station = @way.route[@way.route.index(train_in_station) - 1]
-    puts "Прошлая станция: #{last_station.name}, текущая станция: #{train_in_station.name}, следуюшая станция: #{next_station.name}"
+  def next_station
+    @next_station = @way.route[@way.route.index(current_station) + 1]
+
+  end
+
+  def last_station
+    @last_station = @way.route[@way.route.index(current_station) - 1]
   end
 end
